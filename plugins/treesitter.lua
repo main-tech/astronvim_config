7ae7711f13ac8c533aaf1ff38cd99c0b17fbe0f5
Mandela Mitau <digitalcreative@mandelamitau.com>
2023-09-06 15:15:41 +0300

    add python lsp and hihglihter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,6 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = { "lua", "markdown", "typescript", "tsx", "javascript", "yaml", "dockerfile", "bash", "html",
-      "css", "scss", "json", "terraform" }
+      "css", "scss", "json", "terraform","python" }
   },
 }
